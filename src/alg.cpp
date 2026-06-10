fb1c02fceb753e73787da0966add5e74899448f6
Dodoroshka <yanovaanastasia18@gmail.com>
2026-06-10 12:02:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,6 @@
 // Copyright 2025 NNTU-CS
 #include <cctype>
 #include <map>
-#include <string>
-#include <functional>
 #include "tstack.h"
 std::string infx2pstfx(const std::string& inf) {
     std::map<char, int> priority;
@@ -11,13 +9,17 @@ std::string infx2pstfx(const std::string& inf) {
     priority['*'] = 2;
     priority['/'] = 2;
     priority['('] = 0;
+    
     TStack<char, 100> st;
     std::string res;
+    
     for (size_t i = 0; i < inf.length(); ++i) {
         char c = inf[i];
+        
         if (c == ' ') continue;
-        if (c >= '0' && c <= '9') {
-            while (i < inf.length() && inf[i] >= '0' && inf[i] <= '9') {
+        
+        if (std::isdigit(c)) {
+            while (i < inf.length() && std::isdigit(inf[i])) {
                 res += inf[i];
                 ++i;
             }
@@ -43,7 +45,7 @@ std::string infx2pstfx(const std::string& inf) {
             st.push(c);
         }
     }
-
+    
     while (!st.isEmpty()) {
         if (st.get() != '(') {
             res += st.get();
@@ -51,41 +53,39 @@ std::string infx2pstfx(const std::string& inf) {
         }
         st.pop();
     }
+    
     if (res.length() > 0 && res[res.length() - 1] == ' ') {
         res.resize(res.length() - 1);
     }
+    
     return res;
 }
 
-int add(int a, int b) { return a + b; }
-int sub(int a, int b) { return a - b; }
-int mul(int a, int b) { return a * b; }
-int divv(int a, int b) { return a / b; }
-
 int eval(const std::string& post) {
-    std::map<char, int(*)(int, int)> operations;
-    operations['+'] = add;
-    operations['-'] = sub;
-    operations['*'] = mul;
-    operations['/'] = divv;
     TStack<int, 100> st;
+    
     for (size_t i = 0; i < post.length(); ++i) {
         char c = post[i];
+        
         if (c == ' ') continue;
-
-        if (c >= '0' && c <= '9') {
+        
+        if (std::isdigit(c)) {
             int num = 0;
-            while (i < post.length() && post[i] >= '0' && post[i] <= '9') {
+            while (i < post.length() && std::isdigit(post[i])) {
                 num = num * 10 + (post[i] - '0');
                 ++i;
             }
             st.push(num);
             --i;
-        } else if (operations.find(c) != operations.end()) {
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
             int b = st.get(); st.pop();
             int a = st.get(); st.pop();
-            int result = operations[c](a, b);
-            st.push(result);
+            
+            if (c == '+') 
+                st.push(a + b); else if (c == '-')
+                st.push(a - b); else if (c == '*')
+                    st.push(a * b); else
+                    st.push(a / b);
         }
     }
     return st.get();
